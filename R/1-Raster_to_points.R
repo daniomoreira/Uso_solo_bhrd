7f50fb7ce967c746cb3ea46e6ed5344798d7bcd8
Danielle Moreira <daniomoreira@gmail.com>
2020-03-26 17:54:09 -0300

    adding how saving figure and other small changes in format

--- R/1-Raster_to_points.R
+++ R/1-Raster_to_points.R
@@ -1,21 +1,38 @@
-#################################################
-### SCRIPT PARA TRANSFORMAR RASTER EM PONTOS ####
-### Por: Danielle de O. Moreira              ####
-#################################################
+########################################################
+### SCRIPT TO TRANSFORM RASTER FILE IN SHP (POINTS) ####
+### Por: Danielle de O. Moreira                     ####
+### Data: 26/03/2020                                ####
+########################################################
 
 library(raster)
 library(rgdal)
 
-
+#Creating an object with the raster file
 uso <- raster("./data/mapbiomas-2018-bhrd-1km.tif")
 
+#Information about the file
+uso
+
+#To plot a figure
+plot(uso)
+
+#TO save a figure
+png("figs/figura01.png", res = 300, width = 2400, height = 1200)
+plot(uso)
+dev.off()
+
+#To transform raster to points
 uso.pontos <- rasterToPoints(uso, spatial=T)
 
-#Salvar o arquivo em shp
+uso.pontos
+
+#Saving the shp file
 writeOGR(uso.pontos, "./output", "map-2018-bhrd-1km", driver = "ESRI Shapefile")
 
-plot(uso.pontos)
-
-uso.pontos
+#plot the shp files with the raster file and saving it (only for exercising purpuses)
+png("figs/figura02.png", res = 300, width = 2400, height = 1200)
+plot(uso)
+plot(uso.pontos, add = TRUE)
+dev.off()
 
 ##END
