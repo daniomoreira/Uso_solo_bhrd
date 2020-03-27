697a36ce2973de4d8b41869f2042fe795af0555c
Danielle Moreira <daniomoreira@gmail.com>
2020-03-27 18:53:05 -0300

    function to save shp without zeros values

--- R/1-Raster_to_points.R
+++ R/1-Raster_to_points.R
@@ -22,17 +22,30 @@ plot(uso)
 dev.off()
 
 #To transform raster to points
-uso.pontos <- rasterToPoints(uso, spatial=T)
+uso.pontos1 <- rasterToPoints(uso, spatial = T)
 
-uso.pontos
+#Let's check the new shapefile
 
-#Saving the shp file
-writeOGR(uso.pontos, "./output", "map-2018-bhrd-1km", driver = "ESRI Shapefile")
+plot(uso.pontos1)
+
+# All the raster pixels were converted, including the ones with zero values. We need to remove them. First, let's check the name of the column
+
+names(uso.pontos1)
+
+# Now, let's remove the zero values
+
+uso.pontos2 <- uso.pontos1[uso.pontos1$mapbiomas.2018.bhrd.1km > 0,]
+
+plot(uso.pontos2)
+
+#Saving the new shp file (without zero values)
+writeOGR(uso.pontos2, "./output", "map-2018-bhrd-1km", driver = "ESRI Shapefile", overwrite_layer = T)
 
 #plot the shp files with the raster file and saving it (only for exercising purpuses)
-png("figs/figura02.png", res = 300, width = 2400, height = 1200)
+png("figs/figura02.png", res = 300, width = 2400, height = 1200,)
 plot(uso)
-plot(uso.pontos, add = TRUE)
+plot(uso.pontos2, add = TRUE)
 dev.off()
+?png
 
 ##END
